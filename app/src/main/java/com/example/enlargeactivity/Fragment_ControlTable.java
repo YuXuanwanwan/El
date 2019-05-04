ecefe2a77496bae549a0a6fb65c416a5d6f985b6
LiYuxuan <181250076@smail.nju.edu.cn>
2019-05-05 01:13:40 +0800

    呕心沥血

--- app/src/main/java/com/example/enlargeactivity/Fragment_ControlTable.java
+++ app/src/main/java/com/example/enlargeactivity/Fragment_ControlTable.java
@@ -7,23 +7,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
 public class Fragment_ControlTable extends Fragment {
-    private RelativeLayout mText;
+    private Button btnpop;
+    private PopupWindow popmap;
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.control_table,container,false);
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view=inflater.inflate(R.layout.control_table,container,false);
+        btnpop=(Button)view.findViewById(R.id.btn_map);
+        btnpop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View viewmap=getLayoutInflater().inflate(R.layout.popupwindow_map,null);
+                popmap=new PopupWindow(viewmap,btnpop.getWidth(),ViewGroup.LayoutParams.WRAP_CONTENT);
+                popmap.showAsDropDown(view,50,50);
+            }
+        });
         return view;
         
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
     }
 }
--- app/src/main/java/com/example/enlargeactivity/Fragment_Start.java
+++ app/src/main/java/com/example/enlargeactivity/Fragment_Start.java
@@ -1,10 +1,13 @@
 package com.example.enlargeactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,18 +32,25 @@ public class Fragment_Start extends Fragment {
             public void onClick(View v) { //开始按钮的点击事件
                 Animation animation = AnimationUtils.loadAnimation(view.getContext(), R.anim.enlarge);
                 animation.setFillAfter(false); //最后停留的页面是否为放大后的页面
-                handler.postDelayed(runnable, 1500);//让按钮延迟一秒后消失
+                handler.postDelayed(runnablebigger, 1500);//让按钮延迟一秒后消失
+                handler.postDelayed(runnablechange,4200);
                 imageView.startAnimation(animation);
             }
         });
         return view;
     }
-    Runnable runnable =new Runnable() {
+    Runnable runnablebigger =new Runnable() {
         @Override
         public void run() {
             btnStart.setVisibility(View.GONE);
         }
     };
+    Runnable runnablechange=new Runnable() {
+        @Override
+        public void run() {
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.Container_ControlTable, new Fragment_ControlTable(), null).addToBackStack(null).commit();
+        }
+    };
 }
-
+;
 
